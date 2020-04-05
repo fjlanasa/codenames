e48702767e5b31b54954a6ff36441271056c56e9
Frank LaNasa <fjlanasa@Franks-MacBook-Pro.local>
2020-04-05 19:49:40 -0400

    add httpoison

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Codenames.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:mogrify, "~> 0.7.3"}
+      {:mogrify, "~> 0.7.3"},
+      {:httpoison, "~> 1.6"}
     ]
   end
 
