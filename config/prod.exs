9e5c1f7e2ef0bc48b9ff595291bf8cc0cfe37408
Frank LaNasa <fjlanasa@Franks-MacBook-Pro.local>
2020-04-07 11:01:10 -0400

    buildpack config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,13 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :codenames, CodenamesWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [
+    scheme: "https",
+    host: System.get_env("HOST"),
+    port: String.to_integer(System.get_env("PORT") || "443")
+  ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
